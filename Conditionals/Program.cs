7f09e89647505e032d208d2d2bebee1e68de3595
rebeccaseel <rseelgws@gmail.com>
2017-06-28 14:56:59 -0400

    update2

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -10,7 +10,7 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
-            //ask user for number of diners in their party
+            /*//ask user for number of diners in their party
             Console.WriteLine("How many in your party?");
             double totalNumberofPeople = double.Parse(Console.ReadLine());
 
@@ -36,7 +36,91 @@ namespace Conditionals
                 Console.WriteLine("Your final bill before any discounts is: $" + totalAmount);
                 Console.WriteLine("Your Final Bill after Discount: $" + fiveDiscount);
                 Console.WriteLine("Congratualations! You have received a 5% discount on your bill. Thanks you for your patronage.");
+             
+            }*/
+
+            /*int age;
+
+            Console.WriteLine("What is your age?");
+            age = int.Parse(Console.ReadLine());
+
+            if (age > 0 && age <= 2)
+            {
+                Console.WriteLine("Still in Mama's Arms");
             }
+            else if (age >= 3 && age <= 4)
+            {
+                Console.WriteLine("Preschool Maniac");
+            }
+            else if (age >= 5 && age <= 11)
+            {
+                Console.WriteLine("Elementary School");
+            }
+            else if (age >= 12 && age <= 14)
+            {
+                Console.WriteLine("Middle School");
+            }
+            else if (age >= 15 && age <= 18)
+            {
+                Console.WriteLine("High School");
+            }
+            else if (age >= 19 && age <= 22)
+            {
+                Console.WriteLine("College");
+            }
+            else if (age >= 23 && age <= 65)
+            {
+                Console.WriteLine("Working for the Man");
+            }
+            else if (age >= 66 && age <= 100)
+            {
+                Console.WriteLine("The Golden Years");
+            }
+            else           {
+                Console.WriteLine("This Program is for Humnans");
+          }
+        */
+
+            Console.WriteLine("Type a number between 1 and 10 in word form");
+
+            string word = Console.ReadLine();
+            switch (word)
+            {
+                case "one":
+                    Console.WriteLine("1");
+                    break;
+                case "two":
+                    Console.WriteLine("2");
+                    break;
+                case "three":
+                    Console.WriteLine("3");
+                    break;
+                case "four":
+                    Console.WriteLine("4");
+                    break;
+                case "five":
+                    Console.WriteLine("5");
+                    break;
+                case "six":
+                    Console.WriteLine("6");
+                    break;
+                case "seven":
+                    Console.WriteLine("7");
+                    break;
+                case "eight":
+                    Console.WriteLine("8");
+                    break;
+                case "nine":
+                    Console.WriteLine("9");
+                    break;
+                case "ten":
+                    Console.WriteLine("10");
+                    break;
+                default:
+                    Console.WriteLine("Error: please enter another number between 1 and 10 in word form");
+                    break;
+           }
+
 
         }
     }
